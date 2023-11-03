baf2d5675406ba381acd0008c2f3d0ca4791f81d
Sami Hakani <sami.hakani@gmail.com>
2023-11-03 16:48:58 -0400

    Added docstrings

--- app.py
+++ app.py
@@ -5,11 +5,17 @@ from werkzeug.exceptions import abort
 app = Flask(__name__)
 
 def get_db_connection():
+    """
+    Creates connection to database.
+    """
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row 
     return conn 
 
 def get_post(post_id):
+    """
+    Fetches post with id `post_id`.
+    """
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id)).fetchone()
