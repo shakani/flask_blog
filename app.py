e104ea56dbbd1082e5e3e4cac2e3f12bd63c9099
Sami Hakani <sami.hakani@gmail.com>
2023-11-03 16:48:04 -0400

    Wrote get_post method

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort 
 
 app = Flask(__name__)
 
@@ -7,6 +8,15 @@ def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row 
     return conn 
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 @app.route('/')
 def index():
